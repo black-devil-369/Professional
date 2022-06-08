053c2f7a58c888dbf0240eca068f56a8d8a73682
black-devil-369 <ajaychauhan1262@gmail.com>
2022-06-08 00:47:31 -0400

    coding

---
+++ C++/Cpp_math.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double x, result,result1,result2,result3,result4,result5;
+    cin>>x;
+    result = sinh(x);
+    result1 = cosh(x);
+    result2 = tanh(x);
+    result3 = acos(x);
+    result4 = asinh(x);
+    result5 = atanh(x);
+    cout<<result<<endl;
+    cout<<result1<<endl;
+    cout<<result2<<endl;
+    cout<<result3<<endl;
+    cout<<result4<<endl;
+    cout<<result5<<endl;
+    return 0;
+}
